38609e5783d3a577828c1fdd29b27a3cccacfe9a
Mercedes <meegido@gmail.com>
2020-11-02 13:38:10 +0100

    managing inputs with state and placeholder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import './App.css';
 import React from 'react'
 
+ function Sentence({sentence, onSentenceChange}) {
+    return (
+      <div>
+        <label htmlFor="sentence"></label>
+        <input type="text"  id="sentence" onChange={onSentenceChange} placeholder="Escribe tu idea"/>
+      </div>
+    )
+  }
+
+  function Name({name, onNameChange}) {
+    return (
+      <div>
+        <label htmlFor="name">{name}</label>
+        <input type="text"  id="name" onChange={onNameChange} placeholder="Your name..."/>
+      </div>
+    )
+  }
+
+  function Tags({tags, onTagsChange}) {
+    return (
+      <form >
+        <label htmlFor="tags">{tags}</label>
+        <input type="text" id="tags" onChange={onTagsChange} placeholder="#tags..." />
+      </form>
+    )
+  }
+
 function App() {
   const [sentence, setSentence] = React.useState(' Aquí se ve lo que escribes')
-
-  function handleChange(event) {
-    event.preventDefault()
-    setSentence(event.target.value)
-  }
+  const [tags, setTags] = React.useState(' ')
+  const [name, setName] = React.useState(' ')
 
   return (
     <div className="App">
       <div>
         <h1>{sentence}</h1>
         <div>
-          <p>#tags</p>
+          <Tags tags={tags} onTagsChange={event => setTags(event.target.value)} />
         </div>
-        <div>
-          <p>Tu nombre</p>
-        </div>
-      </div>
-      <div>
         <form>
-          <label htmlFor="sentence">Escribe tu idea</label>
-          <input type="text"  id="sentence" onChange={handleChange}/>
+          <Name name={name} onNameChange={event => setName(event.target.value)}/>
+        </form>
+        <form>
+          <Sentence sentence={sentence} onSentenceChange={event => setSentence(event.target.value)}/>
         </form>
       </div>
     </div>
